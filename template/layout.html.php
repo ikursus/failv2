6c1cb53fb39c8f4e1005f47b32ea660850d5eade
iKursus <ikursus.com@gmail.com>
2021-11-23 12:17:31 +0800

    Update layout

--- template/layout.html.php
+++ template/layout.html.php
@@ -28,7 +28,7 @@
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo dirname(dirname('js/scripts.js')); ?>"></script>
+        <script src="<?php echo dirname(dirname($_SERVER['PHP_SELF'])); ?>/assets/themes/js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
     </body>
